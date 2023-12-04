3c0ed64352c9d8ab23873ecead535cebe0d051ef
ndy2 <emrdbs12@gmail.com>
2023-12-04 21:56:46 +0900

    hello slick - Part 1 CRUD

--- src/main/scala/hello/slick/SlickCRUD.scala
+++ src/main/scala/hello/slick/SlickCRUD.scala
@@ -10,12 +10,14 @@ object PrivateExecutionContext {
   implicit val ec: ExecutionContext = ExecutionContext.fromExecutorService(executors)
 }
 
-object Main {
+object SlickCRUD extends App {
+
   import PrivateExecutionContext._
   import slick.jdbc.PostgresProfile.api._
 
   val shawsharkRedemption = Movie(1L, "The Shawshank Redemption", LocalDate.of(1994, 9, 23), 162)
   val theMatrix = Movie(2L, "The Matrix", LocalDate.of(1999, 3, 31), 134)
+
   def demoInsertMovie(): Unit = {
     val queryDescription = SlickTables.movieTable += theMatrix
     val futureId: Future[Int] = Connection.db.run(queryDescription)
@@ -28,25 +30,40 @@ object Main {
   }
 
   def demoReadAllMovies(): Unit = {
-//    val resultFuture: Future[Seq[Movie]] = Connection.db.run(SlickTables.movieTable.result)
-    val resultFuture: Future[Seq[Movie]] = Connection.db.run(SlickTables.movieTable.filter(_.name.like("%Matrix%")).result)
+    //    val resultFuture: Future[Seq[Movie]] = Connection.db.run(SlickTables.movieTable.result)
+    val resultFuture: Future[Seq[Movie]] =
+      Connection.db.run(SlickTables.movieTable.filter(_.name.like("%Matrix%")).result)
 
     resultFuture.onComplete({
       case Success(movies) => println(s"Fetched ${movies.mkString(", ")}")
       case Failure(ex)     => println(s"Fetching failed, reason: $ex")
     })
+  }
+  def demoUpdateMovie(): Unit = {
+    val resultFuture: Future[Int] =
+      Connection.db.run(
+        SlickTables.movieTable.filter(_.id === 1L).update(shawsharkRedemption.copy(lengthInMin = 150))
+      )
 
-    def demoUpdateMovie(): Unit = {
-      val resultFuture: Future[Seq[Movie]] = Connection.db.run(SlickTables.movieTable.filter(_.name.like("%Matrix%")).result)
-
-      resultFuture.onComplete({
-        case Success(movies) => println(s"Fetched ${movies.mkString(", ")}")
-        case Failure(ex) => println(s"Fetching failed, reason: $ex")
-      })
+    resultFuture.onComplete {
+      case Success(updateMovieId) => println(s"Update was successful, updated id is $updateMovieId")
+      case Failure(ex)            => println(s"Update failed, reason: $ex")
+    }
 
     Thread.sleep(10000)
   }
-  def main(args: Array[String]): Unit = {
-    demoReadAllMovies()
+
+  def demoDeleteMovie(): Unit = {
+    val resultFuture: Future[Int] =
+      Connection.db.run(SlickTables.movieTable.filter(_.id === 2L).delete)
+
+    resultFuture.onComplete {
+      case Success(updateMovieId) => println(s"Delete was successful, updated id is $updateMovieId")
+      case Failure(ex)            => println(s"Delete failed, reason: $ex")
+    }
+
+    Thread.sleep(10000)
   }
+  
+  demoDeleteMovie()
 }
